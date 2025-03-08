12056057e05dfda1e28d83735ffdb04e555eb69a
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-08 15:49:12 +0530

    added get api

--- src/handlers/router.ts
+++ src/handlers/router.ts
@@ -6,6 +6,18 @@ export const defineRoute = (handler: RouteHandler) => handler;
 
 const router = Router();
 
+router.get(
+  "/recipes",
+  defineRoute(async (req, res) => {
+    try {
+      const recipes = await Recipe.find().sort({ category: 1, order: 1 });
+      res.json(recipes);
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
+  })
+);
+
 
 
 export default router;
