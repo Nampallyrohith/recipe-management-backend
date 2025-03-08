b724650a08de845eaf11fd741a04d6b0dfc1166a
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-08 15:49:36 +0530

    added post api

--- src/handlers/router.ts
+++ src/handlers/router.ts
@@ -18,6 +18,32 @@ router.get(
   })
 );
 
+router.post(
+    "/recipes",
+    defineRoute(async (req, res) => {
+      try {
+        const { title, ingredients, instructions, category } = req.body;
+        if (!title || !ingredients.length || !instructions) {
+          return res.status(400).json({ error: "All fields are required" });
+        }
+        const count = await Recipe.countDocuments({ category });
+        const newRecipe = new Recipe({
+          title,
+          ingredients,
+          instructions,
+          category,
+          order: count,
+        });
+        await newRecipe.save();
+        res.status(201).json(newRecipe);
+      } catch (err) {
+        res.status(500).json({ error: err });
+      }
+    })
+  );
+  
+
+
 
 
 export default router;
